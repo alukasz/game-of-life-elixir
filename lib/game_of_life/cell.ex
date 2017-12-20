13f627001d5a03ef85d325811a10b8dc6969dc5b
Łukasz Antończyk <antonczyklukasz@gmail.com>
2017-12-20 23:41:31 +0100

    Name cells using CellsRegistry

--- lib/game_of_life/cell.ex
+++ lib/game_of_life/cell.ex
@@ -1,11 +1,24 @@
 defmodule GameOfLife.Cell do
   use GenServer
 
+  @registry GameOfLife.CellsRegistry
+
   def start_link({_, _} = coords) do
-    GenServer.start_link(__MODULE__, coords)
+    GenServer.start_link(__MODULE__, coords, name: via_tuple(coords))
+  end
+
+  def get({_, _} = coords) do
+    case Registry.lookup(@registry, coords) do
+      [{pid, _}] -> {:ok, pid}
+      _ -> {:error, :not_exists}
+    end
   end
 
   def init(coords) do
     {:ok, %{coords: coords}}
+  end
+
+  defp via_tuple(coords) do
+    {:via, Registry, {@registry, coords}}
   end
 end
---
+++ test/game_of_life/cell_test.exs
@@ -0,0 +1,17 @@
+defmodule GameOfLife.CellTest do
+  use ExUnit.Case
+
+  alias GameOfLife.Cell
+
+  test "get/1 returns cell by coordinates" do
+    coords = {1, 2}
+
+    {:ok, pid} = Cell.start_link(coords)
+
+    assert Cell.get(coords) == {:ok, pid}
+  end
+
+  test "get/2 returns error tuple when cell doesn't exist" do
+    assert Cell.get({1, 2}) == {:error, :not_exists}
+  end
+end
