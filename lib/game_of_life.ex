0d6261e8b17a969c3ee6077e801fc3038a57526b
Łukasz Antończyk <antonczyklukasz@gmail.com>
2017-12-20 23:21:43 +0100

    Clean up generated code

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -1,18 +1,2 @@
 defmodule GameOfLife do
-  @moduledoc """
-  Documentation for GameOfLife.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GameOfLife.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -1,8 +1,3 @@
 defmodule GameOfLifeTest do
   use ExUnit.Case
-  doctest GameOfLife
-
-  test "greets the world" do
-    assert GameOfLife.hello() == :world
-  end
 end
